b202833a2512369efd3028121a8799bd570601fb
Nebojša Ilić <nebojsa.ilic@factoryww.com>
2021-08-06 10:39:34 +0200

    install packages

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1 className="bg-red color-red">dejdowejdweoidjewiodjwo</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
